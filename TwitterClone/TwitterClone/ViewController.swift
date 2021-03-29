d6d46e702edde1e088edfd84e5d3d501360248a7
awan-ur-rahman <awanrahman55@gmail.com>
2021-03-29 02:54:27 -0700

    project created

---
+++ TwitterClone/TwitterClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterClone
+//
+//  Created by aawan on 29/3/21.
+//  Copyright © 2021 aawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
