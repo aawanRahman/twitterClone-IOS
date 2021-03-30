1fbec20e454ed0f2730d1b6b494534087933b5da
awan-ur-rahman <awanrahman55@gmail.com>
2021-03-30 04:57:55 -0700

    user sign up

--- TwitterClone/TwitterClone/SignUpViewController.swift
+++ TwitterClone/TwitterClone/SignUpViewController.swift
@@ -11,6 +11,20 @@ import FirebaseDatabase
 
 import  FirebaseAuth
 
+
+class FirebaseAuthManager {
+    func createUser(email: String, password: String, completionBlock: @escaping (_ success: Bool) -> Void) {
+        Auth.auth().createUser(withEmail: email, password: password) {(authResult, error) in
+            if let user = authResult?.user {
+                print(user)
+                completionBlock(true)
+            } else {
+                completionBlock(false)
+            }
+        }
+    }
+}
+
 class SignUpViewController: UIViewController {
 
     @IBOutlet weak var email: UITextField!
@@ -18,9 +32,15 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var password: UITextField!
     
     @IBOutlet weak var signUp: UIBarButtonItem!
+   // var ref: DatabaseReference!
+    @IBOutlet weak var errorMessage: UILabel!
+    
+    var ref = Database.database().reference()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        signUp.isEnabled = false
 
         // Do any additional setup after loading the view.
     }
@@ -36,11 +56,54 @@ class SignUpViewController: UIViewController {
     }
     */
     
-    @IBAction func didTapCancel(_ sender: Any) {
+    func createAlert(title: String, message: String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: {(action) in
+            
+            alert.dismiss(animated: true, completion: nil)
+            
+            
+        }))
+        self.present(alert, animated: true, completion: nil)
+        
+     
     }
     
-    @IBAction func didTapSignUp(_ sender: Any) {
+    @IBAction func didTapCancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
         
     }
     
+    @IBAction func didTapSignUp(_ sender: Any) {
+        signUp.isEnabled = false
+        
+        let signUpManager = FirebaseAuthManager()
+        if let email = email.text, let password = password.text {
+            signUpManager.createUser(email: email, password: password) {[weak self] (success) in
+                guard let `self` = self else { return }
+                var message: String = ""
+                if (success) {
+                    message = "User was sucessfully created."
+                } else {
+                    message = "There was an error."
+                }
+                let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: .cancel, handler: nil))
+                self.createAlert(title: "Sign Up!!", message: "User is successfully created")
+                //display(alertController: alertController)
+            }
+        }
+        
+        
+    }
+    
+    @IBAction func didTextChanged(_ sender: UITextField) {
+        if(!email.text!.isEmpty == true && !password.text!.isEmpty==true) {
+            signUp.isEnabled = true
+            
+        }
+        else {
+            signUp.isEnabled = false
+        }
+    }
 }
